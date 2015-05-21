78c5fa831afc1df91460e8950fbd909aad6a6f6a
unknown <th05@PC4126.corp.cefas.co.uk>
2015-05-21 16:23:52 +0100

    split deployment.map into bathymap

--- R/geo.R
+++ R/geo.R
@@ -27,7 +27,6 @@ deployment.map <- function(platforms = c(1, 4, 8),
     require(ggplot2)
     require(data.table)
     require(lubridate)
-    require(ggmap)
 
     sbdb= odbcConnect("SmartbuoydbLive")
 
@@ -44,8 +43,8 @@ deployment.map <- function(platforms = c(1, 4, 8),
     d = d[platform %in% c(1, 4, 8)] # remove non-standard deployments
     d = d[!(groupId %in% c('LOWTEST', 'ESM2TEST'))] # remove test deployments
     d = d[,list(lat = median(lat), lon = median(long), dateTo = max(dateTo), platform = platform[1]), by = groupId] # group by deployment
-    d$active = "Historical"
-    d$active[d$dateTo > now()] = "Current"
+    d$active = "inactive"
+    d$active[d$dateTo > now()] = "active"
     
     d[platform == 1, platformName := 'SmartBuoy']
     d[platform == 1, shape := 2]
@@ -68,17 +67,21 @@ deployment.map <- function(platforms = c(1, 4, 8),
     if(zoom_to_group == FALSE){
         centre.lat = 53
         centre.lon = -2.7
+        max.range = 14
         zoom = 6
     }else{
         centre.lat = mean(range(d$lat))
         centre.lon = mean(range(d$lon))
         max.range = max(diff(range(d$lon)), diff(range(d$lat)))
+        # add 5% buffer
+        max.range = max.range + max.range * 0.05
         zoom = max(ranges$zoom[ranges$range >= max.range])
     }
     
     centre = c(centre.lon, centre.lat)
     
     if(style == 'gsat'){
+    require(ggmap)
     mp = ggmap(get_map(location = centre, zoom = zoom, maptype = 'satellite'))
     mp = mp + geom_point(data = d, aes(lon, lat, color = active, position = 'jitter', shape = as.factor(platform)), size = 2.5) +
         scale_color_discrete('') + scale_shape_manual('', values = unique(d$shape), labels = unique(d$platformName), drop = F) +
@@ -88,4 +91,37 @@ deployment.map <- function(platforms = c(1, 4, 8),
     }
     
     return(list(map = mp, data = d))
+}
+
+Bathymap <- function(){
+    # stuff
+        require(raster)
+        require(rgdal)
+        require(mapdata)
+        require(RColorBrewer)
+        xlim = c(centre.lon - (max.range / 2), centre.lon + (max.range / 2))
+        ylim = c(centre.lat- (max.range / 2), centre.lat+ (max.range / 2))
+        # make bathy
+        bathy = raster(bathy_file)
+        
+        bathy = crop(bathy, extent(c(xlim, ylim)))
+        rtp = data.frame(rasterToPoints(bathy))
+        colnames(rtp) = c('x', 'y', 'h')
+        # classify
+        rtp$c = cut(rtp$h, breaks = c(1, -25, -50, -100, -200, -5000), labels = 1:5)
+        # make geom
+        bathy_raster = geom_raster(data = rtp, aes(x, y, fill = c))
+        
+        coast = map_data('worldHires', xlim = xlim, ylim = ylim)
+        coast.poly <- geom_polygon(data=coast, aes(x=long, y=lat, group=group), colour= "#999999", fill="#999999", lwd=0.2)
+        coast.outline <- geom_path(data=coast, aes(x=long, y=lat, group=group), colour= "#999999", lwd=0.2)
+        
+        mp = ggplot() + bathy_raster + coast.poly + coast.outline +
+            coord_quickmap(xlim, ylim) +
+            scale_fill_manual(values = rev(brewer.pal(5,"Blues")),name='depth', labels=rev(c('<25','25-50','50-100','100-200','>200')))
+        mp = mp + geom_point(data = d, aes(lon, lat, color = active, position = 'jitter', shape = as.factor(platform)), size = 2.5) +
+            scale_color_discrete('') + scale_shape_manual('', values = unique(d$shape), labels = unique(d$platformName), drop = F) +
+            labs(x = 'Longitude', y = 'Latitude')
+            
+        
 }
