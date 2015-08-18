7eb72c89196a9aba45e83eac4827fe6ab8187fff
Tom Hull <tom.hull@cefas.co.uk>
2015-08-18 15:46:57 +0100

    fixes, and added historic lims function

--- R/lims.R
+++ R/lims.R
@@ -1,5 +1,8 @@
 #' Fetch nutrients data from LIMS (post 2013)
 #'
+#' @details parameter codes:
+#' Chlorophyll, TOXN, O2, SLD, SAL
+#' 
 #' @param parameters 
 #' @param cruise 
 #' @param db_name
@@ -14,9 +17,11 @@ lims.fetch <- function(parameters = 'SAL', cruise = NA, db_name = 'lims'){
                   "[C_LATITUDE] as latitude,",
                   "[C_LONGITUDE] as longitude,",
                   "[C_CRUISE_CODE] as cruise,",
+                  "[C_STATION] as station,",
                   "[NAME] as par,",
                   "[UNITS] as unit,",
-                  "[ENTRY] as value",
+                  "[ENTRY] as value,",
+                  "[C_SAMPLE_DEPTH] as depth",
                   "FROM C_NUTRIENTS_SAMPLES")
     
         # collapse down parameters vector and wrap with quotes to work with IN (xxx)
@@ -34,11 +39,57 @@ lims.fetch <- function(parameters = 'SAL', cruise = NA, db_name = 'lims'){
     
     lims = odbcConnect(db_name)
     dat =  data.table(sqlQuery(lims, query))
+    # check if valid data has been returned, if not quit
+    if(! nrow(dat) > 1){
+        stop("no data returned")
+    }
     dat[,longitude := as.numeric(as.character(longitude))] # lat and long are stored as varchar15
     dat[,latitude := as.numeric(as.character(latitude))]
     dat[,value := as.numeric(as.character(value))]
     odbcCloseAll()
     return(dat)
+}
+
+#' Fetch nutrients data from LIMS (pre 2013)
+#'
+#' @param parameters 
+#' @param db_name
+#'
+#' @return data frame (data.table) of extracted nutrients data
+#' @export
+lims.fetch.historic <- function(parameters = 'SAL', db_name = 'lims'){
+    require(RODBC)
+    require(data.table)
+    
+        query = paste("SELECT [date_collected] as dateTime,",
+                  "[latitude] as latitude,",
+                  "[longitude] as longitude,",
+                  "[station] as station,",
+                  "[ParamId] as par,",
+                  "[EnteredUnits] as unit,",
+                  "[EnteredValue] as value,",
+                  "[pressure] as depth",
+                  "FROM C_Historic_Nutrient_Samples")
+    
+        # collapse down parameters vector and wrap with quotes to work with IN (xxx)
+    parameters_fetch = paste(parameters, collapse = "', '")
+    query = paste0(query, " WHERE [ParamId] IN ('", parameters_fetch, "')")
+    
+    query = paste(query, 'ORDER BY dateTime')
+    print(query)
+    
+    lims = odbcConnect(db_name)
+    dat =  data.table(sqlQuery(lims, query))
+    # check if valid data has been returned, if not quit
+    if(! nrow(dat) > 1){
+        stop("no data returned")
+    }
+#     dat[,longitude := as.numeric(as.character(longitude))] # lat and long are stored as varchar15
+#     dat[,latitude := as.numeric(as.character(latitude))]
+#     dat[,value := as.numeric(as.character(value))]
+    odbcCloseAll()
+    return(dat)
+    
 }
 
 #' Fetch list of cruises from LIMS
