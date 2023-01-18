0468c1ee1f9c52772c56b189879288cda3b0a793
Tom Hull <tom.hull@cefas.gov.uk>
2023-01-18 16:37:21 +0000

    rename wavenet.sbd_info, add ability to decode air temperature

--- R/wavenet.R
+++ R/wavenet.R
@@ -66,41 +66,52 @@ wavenet.fetch <- function(deployment = NA, deployment_group = NA,
 #' Decode DWR3 sbd file info
 #'
 #' @param sbd waverider sbd filename
+#' @param debug if True, print matching messages
 #'
 #' @return list with named elements of lat, lon and battery (in weeks)
 #'
-wavenet.sbd_info <- function(sbd){
+read.wavenet_sbd_info <- function(sbd, debug = F){
   sbd = readBin(sbd, what = "raw", n = 128)
 
   which_msg <- function(byte){
     # checks id nibble from byte, returns 2 element vector or message ID and size of message
     # first element nibble
-    nibble = paste0(rawToBits(byte)[5:8], collapse = " ")
-    switch(nibble,
-           "00 00 00 00" = c(0, 31), # message 0 (31 bytes) heave
-           "01 01 00 00" = c(3, 11), # message 3 (11 bytes) spectral param
-           "01 00 01 00" = c(5, 10), # message 5 (10 bytes) info
-           "00 01 01 00" = c(6, 10), # message 6 (10 bytes) sea surface temp
-           "01 00 00 01" = c(9, 31), # message 9 (31 bytes) compressed directional spectrum
-           c(NA, NA)) # message 6 (10 bytes) temp
+    HiNibble = paste0(rawToBits(byte)[5:8], collapse = " ")
+    switch(HiNibble,
+           "00 00 00 00" = c("0", 31), # message 0 (31 bytes) heave
+           "01 01 00 00" = c("3", 11), # message 3 (11 bytes) spectral param
+           "01 00 01 00" = c("5", 10), # message 5 (10 bytes) info
+           "00 01 01 00" = c("6", 10), # message 6 (10 bytes) sea surface temp
+           "01 00 00 01" = c("9", 31), # message 9 (31 bytes) compressed directional spectrum
+           "00 01 00 01" = c("A", 10), # message A (10 bytes) air temperature
+           c(NA, NA))
   }
 
   # starting with first byte,
   # figure out which message it is and increment using known message lengths until we find info message
   i = 1
   info_index = NA
-  temp_index = NA
+  sst_index = NA
+  airtemp_index = NA
   while(i < length(sbd)){
     res = which_msg(sbd[i])
-    if(anyNA(res)){stop("message type not recognised")}
-    if(res[1] == 5){
+    if(debug){
+      print(c(paste0(rawToBits(sbd[i])[5:8], collapse = " "), res)) # debug
+    }
+    if(anyNA(res)){stop(paste("message type not recognised at index "), i)}
+    if(res[1] == "5"){
       info_index = i
     }
-    if(res[1] == 6){
-      temp_index = i
+    if(res[1] == "6"){
+      sst_index = i
     }
-    i = i + res[2]
+    if(res[1] == "A"){
+      airtemp_index = i
+    }
+    i = i + as.numeric(res[2])
   }
+
+  raw_to_dec <- function(x)  sum(2^(which(rev(unlist(strsplit(as.character(x), "")) == 1))-1))
 
   info = sbd[info_index:(info_index+10)]
   info = as.numeric(info)[-1]
@@ -108,5 +119,13 @@ wavenet.sbd_info <- function(sbd){
   lon = 180 * (info[4] * 0x10000 + info[5] * 0x100 + info[6])/2^23
   lon[lon > 180] = lon[lon > 180] - 360
   batt = info[7]
+  if(is.na(sst_index)){
+    sst = NA
+  }else{
+    sst = sbd[(sst_index+1):(sst_index+2)]
+    sst = rawToBits(sst)[1:10]
+    sst = raw_to_dec(sst)/20 - 5
+  }
+
   return(list(lat = lat, lon = lon, batt = batt))
 }
